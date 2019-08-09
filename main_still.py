7c88d5c8042e464141c0af823d9ee15909f5399e
FredOleary <fred@murphyoleary.com>
2019-08-09 14:20:11 -0700

    initial file added

---
+++ main_still.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  main.py
+#  
+#  Copyright 2019  <pi@raspberrypi>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+import cv2
+
+
+def main(args):
+	print("hello")
+	face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml' )
+
+	img = cv2.imread("./fred_still.png", -1)
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	
+	faces = face_cascade.detectMultiScale(gray, 1.3, 1, minSize=(10,10))
+	for (x,y,w,h) in faces:
+		cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+				
+
+	cv2.imshow("foo", img)
+	cv2.waitKey(0)
+	return 0
+#	img = cv2.imread("/home/pi/projects/test_opencv/Compass.png", -1)
+#	cv2.imshow("foo1", img)
+	
+#	cap = cv2.VideoCapture('Sample-H264-1.mp4')
+
+	
+	face_cascade = cv2.CascadeClassifier('/usr/share/opencv/lbpcascades/lbpcascade_frontalface.xml')
+	cap = cv2.VideoCapture("Fred3.mov")
+	
+	# Check if camera opened successfully
+	if (cap.isOpened()== False): 
+		print("Error opening video stream or file")
+ 
+	# Read until video is completed
+	while(cap.isOpened()):
+		# Capture frame-by-frame
+		ret, frame = cap.read()
+		if ret == True:
+ 
+			# Display the resulting frame
+			frame = cv2.flip( frame,0 )
+			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+			for (x,y,w,h) in faces:
+				icv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
+				
+			cv2.imshow('Frame',frame)
+	
+			# Press Q on keyboard to  exit
+			if cv2.waitKey(25) & 0xFF == ord('q'):
+				break
+ 
+		# Break the loop
+		else: 
+			break
+ 
+	# When everything done, release the video capture object
+	cap.release()
+	
+	cv2.waitKey(0)
+	return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
