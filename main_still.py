9090fa6afa32f857bd6d16e80b60e91975eca70c
fredoleary <fred@essentel.com>
2019-08-26 17:45:06 -0700

    cleanup

--- main_still.py
+++ main_still.py
@@ -25,62 +25,62 @@ import cv2
 
 
 def main(args):
-	print("hello")
-	face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml' )
+    print("hello")
+    face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml')
 
-	img = cv2.imread("./fred_still.png", -1)
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	
-	faces = face_cascade.detectMultiScale(gray, 1.3, 1, minSize=(10,10))
-	for (x,y,w,h) in faces:
-		cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
-				
+    img = cv2.imread("./fred_still.png", -1)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-	cv2.imshow("foo", img)
-	cv2.waitKey(0)
-	return 0
-#	img = cv2.imread("/home/pi/projects/test_opencv/Compass.png", -1)
-#	cv2.imshow("foo1", img)
-	
-#	cap = cv2.VideoCapture('Sample-H264-1.mp4')
+    faces = face_cascade.detectMultiScale(gray, 1.3, 1, minSize=(10, 10))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-	
-	face_cascade = cv2.CascadeClassifier('/usr/share/opencv/lbpcascades/lbpcascade_frontalface.xml')
-	cap = cv2.VideoCapture("Fred3.mov")
-	
-	# Check if camera opened successfully
-	if (cap.isOpened()== False): 
-		print("Error opening video stream or file")
- 
-	# Read until video is completed
-	while(cap.isOpened()):
-		# Capture frame-by-frame
-		ret, frame = cap.read()
-		if ret == True:
- 
-			# Display the resulting frame
-			frame = cv2.flip( frame,0 )
-			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-			faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-			for (x,y,w,h) in faces:
-				icv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
-				
-			cv2.imshow('Frame',frame)
-	
-			# Press Q on keyboard to  exit
-			if cv2.waitKey(25) & 0xFF == ord('q'):
-				break
- 
-		# Break the loop
-		else: 
-			break
- 
-	# When everything done, release the video capture object
-	cap.release()
-	
-	cv2.waitKey(0)
-	return 0
+    cv2.imshow("foo", img)
+    cv2.waitKey(0)
+    return 0
+    #	img = cv2.imread("/home/pi/projects/test_opencv/Compass.png", -1)
+    #	cv2.imshow("foo1", img)
+
+    #	cap = cv2.VideoCapture('Sample-H264-1.mp4')
+
+    face_cascade = cv2.CascadeClassifier('/usr/share/opencv/lbpcascades/lbpcascade_frontalface.xml')
+    cap = cv2.VideoCapture("Fred3.mov")
+
+    # Check if camera opened successfully
+    if cap.isOpened() == False:
+        print("Error opening video stream or file")
+
+    # Read until video is completed
+    while cap.isOpened():
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret:
+
+            # Display the resulting frame
+            frame = cv2.flip(frame, 0)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            for (x, y, w, h) in faces:
+                icv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+            cv2.imshow('Frame', frame)
+
+            # Press Q on keyboard to  exit
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
+        # Break the loop
+        else:
+            break
+
+    # When everything done, release the video capture object
+    cap.release()
+
+    cv2.waitKey(0)
+    return 0
+
 
 if __name__ == '__main__':
     import sys
+
     sys.exit(main(sys.argv))
